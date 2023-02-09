cb23105136252f9a70405c1aabc8be33bbbd3588
Joao Pedro Henrique <joao.pedro.hsd@gmail.com>
2023-02-09 14:12:20 -0300

    refactor: update structure to use standalone components

--- apps/web-ui/src/app/app.component.ts
+++ apps/web-ui/src/app/app.component.ts
@@ -1,10 +1,10 @@
 import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'posts-challenge-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css'],
+  standalone: true,
+  imports: [RouterModule],
+  template: '<router-outlet></router-outlet>',
 })
-export class AppComponent {
-  title = 'web-ui';
-}
+export class AppComponent {}
--- apps/web-ui/src/main.ts
+++ apps/web-ui/src/main.ts
@@ -1,7 +1,9 @@
-import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+import { bootstrapApplication } from '@angular/platform-browser';
+import { provideRouter, Route } from '@angular/router';
+import { AppComponent } from './app/app.component';
 
-import { AppModule } from './app/app.module';
+export const ROUTES: Route[] = [];
 
-platformBrowserDynamic()
-  .bootstrapModule(AppModule)
-  .catch((err) => console.error(err));
+bootstrapApplication(AppComponent, {
+  providers: [provideRouter(ROUTES)],
+});
