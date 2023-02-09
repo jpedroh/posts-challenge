b579290691f127c871ba269b29f5b706c699a201
Joao Pedro Henrique <joao.pedro.hsd@gmail.com>
2023-02-09 18:37:32 -0300

    feat: add pagination of posts

--- libs/posts/posts-list/src/container/posts-list-container.component.ts
+++ libs/posts/posts-list/src/container/posts-list-container.component.ts
@@ -3,24 +3,30 @@ import { HttpClientModule } from '@angular/common/http';
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { Post } from '@posts-challenge/posts/core';
+import { NgxPaginationModule } from 'ngx-pagination';
 import { PostsListFacade } from '../services/posts-list.facade';
 import { PostsListService } from '../services/posts-list.service';
 
 @Component({
   selector: 'posts-challenge-posts-list',
   standalone: true,
-  imports: [CommonModule, HttpClientModule, RouterModule],
+  imports: [CommonModule, HttpClientModule, RouterModule, NgxPaginationModule],
   providers: [PostsListService, PostsListFacade],
   templateUrl: './posts-list-container.component.html',
   styleUrls: ['./posts-list-container.component.css'],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class PostsListContainerComponent {
+  currentPage = 0;
   readonly posts$ = this.facade.posts$;
 
   constructor(private readonly facade: PostsListFacade) {}
 
   getPostLink(post: Post) {
     return ['/posts', post.id];
+  }
+
+  pageChanged(page: number) {
+    this.currentPage = page;
   }
 }
